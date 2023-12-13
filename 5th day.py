949b504dfc1712c70c2ca92da837abc772cc8143
SHAIK KHASIM  BASHA <115825218+khasimbasha5757@users.noreply.github.com>
2023-12-13 15:40:19 +0530

    typecasting

---
+++ 5th day.py
@@ -0,0 +1,11 @@
+# typecasting: which is called changing one datatype into another data type
+#explicit typecasting : which is done by the programmer by using builtin datatypes;
+a="1"
+# a=1
+b="2"
+# b=2
+print(int(a)+int(b)) 
+# implicit typecasting done by the comnpiler itself by taking the precedence;
+a=1.1
+b=9
+print(a+b) #here we get to know that the answer is in float as it have higher precedence;
